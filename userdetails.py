1374d71252e045d5ed65252dd04b9e5b759fd061
basu <rajbaumeti@gmail.com>
2026-09-01 10:22:48 +0530

    save this file in the branching

--- userdetails.py
+++ userdetails.py
@@ -2,4 +2,4 @@ firstName=input("enter your firstname")
 lastName=input("enter your lastname")
 city=input("enter your city")
 
-print("your Details: ",firstNam+" "+lastName+" "+city)
+print("your Details: ",firstName+" "+lastName+" "+city)
